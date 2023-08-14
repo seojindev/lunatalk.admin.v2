4c13a7b26da552cd8329dd856a94103c703df774
Theo <kkw.theo@gmail.com>
2023-08-14 17:25:14 +0900

    NotFound

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,5 +1,31 @@
+<script setup lang="ts"></script>
 <template>
-  <div class="d-flex h-100 justify-center align-center w-100">
-    <h2>페이지가 존재 하지 않습니다.</h2>
+  <div class="d-flex h-100 justify-center align-center w-100 flex-column">
+    <h1>
+      <span>4</span>
+      <span>0</span>
+      <span>4</span>
+    </h1>
+    <p>페이지를 찾을수 없습니다.</p>
+    <RouterLink to="/">메인으로 이동하기</RouterLink>
   </div>
 </template>
+
+<style scoped>
+h1 {
+  font-size: 150px;
+  font-weight: 800;
+  color: #262626;
+  text-transform: uppercase;
+  letter-spacing: -10px;
+  margin-left: -20px;
+}
+h1 > span {
+  text-shadow: -8px 0 0 #fff;
+}
+
+a {
+  color: #262626;
+  letter-spacing: -1px;
+}
+</style>
