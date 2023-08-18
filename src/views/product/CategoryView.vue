7231b574dcd472242fe0d6e3ea58a4be294e47de
Theo <kkw.theo@gmail.com>
2023-08-18 07:46:58 +0000

    category

--- src/views/product/CategoryView.vue
+++ src/views/product/CategoryView.vue
@@ -1,8 +1,8 @@
 <template>
   <div v-if="!list.length">loading</div>
-  <div class="w-100 pa-2 d-flex flex-column py-10" v-else>
+  <v-layout class="w-100 pa-2 d-flex flex-column py-10" v-else>
     <div class="mb-5 d-flex justify-end" style="gap: 10px">
-      <v-btn color="#5865f2" @click="onAddHandler">추가</v-btn>
+      <v-btn color="#5865f2" @click="onAddHandler" variant="tonal">추가</v-btn>
     </div>
     <v-divider></v-divider>
     <v-table class="w-100 pa-3">
@@ -10,20 +10,16 @@
         <th>고유 번호</th>
         <th>카테고리 명</th>
         <th>상품 갯수</th>
-        <th>액션</th>
       </thead>
       <tbody>
-        <tr v-for="item in list" :key="item.uuid" class="">
+        <tr v-for="item in list" :key="item.uuid">
           <td class="text-center">{{ item.uuid }}</td>
           <td class="text-center">{{ item.name }}</td>
           <td class="text-center">{{ item.products_count }}</td>
-          <td class="d-flex justify-center align-center">
-            <v-btn color="#5865f2" variant="tonal">삭제</v-btn>
-          </td>
         </tr>
       </tbody>
     </v-table>
-  </div>
+  </v-layout>
 </template>
 
 <script setup lang="ts">
@@ -40,14 +36,23 @@ interface CategoryList {
 }
 
 const list = ref<CategoryList[]>([])
+const deleteModal = ref(false)
 
 const onAddHandler = () => {
   router.push('/product/category/create')
 }
 
+const close = () => {
+  deleteModal.value = false
+}
+
+const open = () => {
+  console.log(1)
+  deleteModal.value = true
+}
+
 const getList = async () => {
   const response = await getCategoryList()
-  console.log(response)
   if (response.status === 200) {
     list.value = response.data.result
   }
